719b73f9b016e1a0c01de073f850c753cf4dc9f6
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-05 23:48:14 +0000

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -38,7 +38,7 @@ expenses.read_expenses('data/spending_data.csv')
     ))
 
     fix, ax = plt.subplots()
-    labels = ['Necessary', 'Food', 'Unecessary']
+    labels = ['Necessary', 'Food', 'Unnecessary']
     divided_expenses_sum = []
     for category_exps in divided_set_comp:
         divided_expenses_sum.append(sum( x.amount for x in category_exps ))    
