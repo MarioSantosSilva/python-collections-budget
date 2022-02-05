c62f0421df1e539f49a5df67dba5a4257f21f1e2
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-05 18:53:26 +0000

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -18,13 +18,33 @@ def main():
     print(timeit.timeit(stmt='expenses.categorize_for_loop()', 
     setup = 
     '''
-from . import Expenses
+from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
     ''',
     number =100000,
     globals=globals()
     ))
+
+    print(timeit.timeit(stmt='expenses.categorize_set_comprehension()', 
+    setup = 
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number =100000,
+    globals=globals()
+    ))
+
+    fix, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unecessary']
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum( x.amount for x in category_exps ))    
+
+    ax.pie(divided_expenses_sum, labels = labels, autopct='%1.1f%%')
+    plt.show()
 
 if __name__ == "__main__":
     main()
